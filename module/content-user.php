1bda795fe5adee6bd5cdd4682f7ed4713f26fe20
kun <kunwidy@gmail.com>
2013-01-30 01:27:30 -0800

    Create content-user.php

---
+++ module/content-user.php
@@ -0,0 +1,8 @@
+<li class="span3 content-user">
+  <div class="user-pics">
+		<a href="single-user.php"><img src="upload/relawan.jpg" alt=""></a>
+	</div>
+	<div class="user-name">
+		<a href="single-user.php">SIGIT SEDAYU</a>
+	</div>
+</li>
