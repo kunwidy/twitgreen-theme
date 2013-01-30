4f4c4d13fde12ec6952e961eb7178d3405b4b807
kun <kunwidy@gmail.com>
2013-01-30 01:26:21 -0800

    Create archive-user.php

---
+++ archive-user.php
@@ -0,0 +1,28 @@
+<?php
+  include 'module/header-inside.php';
+?>
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				<h2>DAFTAR USER</h2>
+				<button class="btn btn-success btn-small" type="button"> Semua</button> <button class="btn btn-success btn-small" type="button"> Investor</button> <button class="btn btn-success btn-small" type="button"> Relawan</button> <button class="btn btn-success btn-small" type="button"> Petani</button> <button class="btn btn-success btn-small" type="button"> Pemilik lahan</button>
+				
+				<ul id="list-user" class="thumbnails">
+					<?php
+						$i = 0;
+						while ( $i <= 30) {
+							include 'module/content-user.php';
+							$i ++;
+						}
+					?>
+				</ul>
+				
+				<?php include 'module/pagination.php'; ?>
+			</div>
+		
+	</div>
+	
+<?php
+	include 'module/search-mobile.php';
+	include 'module/footer.php';
+?>
