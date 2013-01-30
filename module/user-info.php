629bb33e048e7c95b5eb0076f4812f4d26a6bb08
kun <kunwidy@gmail.com>
2013-01-30 17:14:46 +0700

    Update module/user-info.php

--- module/user-info.php
+++ module/user-info.php
@@ -1,23 +1,23 @@
-  	
+		
 					<!-- user info -->
 					<div id="user-info" class="side-content">
 						<div class="head">
-							<span class="img-personal"><img src="upload/relawan.jpg" alt="name"></span>
-							<span class="name"><a href="#">Sigit Sedayu</a></span>
+							<img src="upload/relawan.jpg" alt="name">
+							<h3>Sigit Sedayu</h3>
 						</div>
 						<div class="body">
 							<ul>
 							<li><a href="">Facebook Profile</a></li>
 							<li><a href="">Twitter Page</a></li>
-							<li><a href="">ID # : 1761</a></li>
-							<li><a href="">Lokasi : Bandung</a></li>
-							<li><a href="">Bergabung Sejak 2012-10-17 20:24:13</a></li>
+							<li>ID # : 1761</li>
+							<li>Lokasi : Bandung</li>
+							<li>Bergabung Sejak 2012-10-17 20:24:13</li>
 							</ul>
 							
 							<ul>
 								<h5>Statistik :</h5>
-								<li><a href="">Jumlah Lot : 6 Lot</a></li>
-								<li><a href="">Jumlah Pohon : 340 Pohon</a></li>
+								<li>Jumlah Lot : 6 Lot</li>
+								<li>Jumlah Pohon : 340 Pohon</li>
 							</ul>
 						</div>
 					</div>
