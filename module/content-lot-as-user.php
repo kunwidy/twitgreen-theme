b92b41b570c5f2538f1d20428618cf284af7a0d7
kun <kunwidy@gmail.com>
2013-01-31 15:13:33 -0800

    Create content-lot-as-user.php

---
+++ module/content-lot-as-user.php
@@ -0,0 +1,7 @@
+<li class="span3 content-lot">
+  <div class="banner-box center">
+		<p id="lot-name" class="big"><a href="single-lot.php">Cilebak Saat 5 (9) </a></p>
+		<a href="single-lot.php"><img src="upload/new-project.jpg" alt="image01" /></a>
+	</div>
+	<div class="shadow"><img src="img/mini-shadow.png"></div>
+</li>
