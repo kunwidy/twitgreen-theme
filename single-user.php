51945b31b3b9079d93ba385e091be798cf0b40cb
kun <kunwidy@gmail.com>
2013-02-01 06:19:49 +0700

    Update single-user.php

--- single-user.php
+++ single-user.php
@@ -1,12 +1,14 @@
 <?php
-  include 'module/header-inside.php';
+	include 'module/header2.php';
 ?>
 	<div class="container">
 		<div class="row">
-			<div class="span9">
+			<div class="">
 				<h2>Daftar Lot Sigit Sedayu Sebagai</h2>
 				<p>Lihat sebagai : <button class="btn btn-success btn-small" type="button"><i class="icon-globe"></i> Relawan</button> <button class="btn btn-primary btn-small" type="button"><i class="icon-lightbulb"></i> Investor</button>
-				
+			</div>
+			<div class="row">
+			<div class="span9">
 				<ul id="new-projects" class="thumbnails">
 					<?php
 						$i = 0;
@@ -19,15 +21,19 @@
 				
 				<?php include 'module/pagination.php'; ?>
 			</div>
+			
 		
 			<div class="span3">
-				<?php include 'module/user-info.php'; ?>
-			</div>
-			<!-- sidebar widget -->
-				<div id="sidebar" class="span3">
+				<?php include 'module/user-info.php';?>
+				<!-- sidebar widget -->
+				<div id="sidebar">
 					<?php include 'module/widget-filter-by-status.php';?>
 				</div>
 			<!-- /sidebar widget -->
+				
+			</div>
+			
+			</div>
 		</div>
 	</div>
 	
