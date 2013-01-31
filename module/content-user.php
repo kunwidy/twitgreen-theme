c2d1c0b9e1b9be563f4e1f0a81bd5a71ae70a737
kun <kunwidy@gmail.com>
2013-02-01 06:14:26 +0700

    Update module/content-user.php

--- module/content-user.php
+++ module/content-user.php
@@ -1,5 +1,5 @@
 <li class="span3 content-user">
-  <div class="user-pics">
+	<div class="user-pics">
 		<a href="single-user.php"><img src="upload/relawan.jpg" alt=""></a>
 	</div>
 	<div class="user-name">
