c69fe3088f4681d87780507ed052a22d2c2bec5e
kun <kunwidy@gmail.com>
2013-01-31 19:16:24 -0800

    Create page-relawan-registration.php

---
+++ page-relawan-registration.php
@@ -0,0 +1,14 @@
+<?php
+  include 'module/header2.php';
+?>
+	<div class="container">
+		<div class="row">
+			
+			<?php include 'form/form-relawan-registration.php'?>
+		</div>
+	</div>
+	
+<?php
+	include 'module/search-mobile.php';
+	include 'module/footer.php';
+?>
