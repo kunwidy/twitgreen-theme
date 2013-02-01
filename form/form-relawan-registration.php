957307e802155816b4aa27fbbc8fd727bdfbdc4b
kun <kunwidy@gmail.com>
2013-01-31 19:17:00 -0800

    Create form-relawan-registration.php

---
+++ form/form-relawan-registration.php
@@ -0,0 +1,101 @@
+            <div class="span12"><h2>Pendaftaran Relawan</h2></div>
+<!--form relawan registration-->
+    
+            <div class="span9">
+  	
+               <form class="form-horizontal well">
+		    <h3 class="center">
+			FORMULIR PENDAFTARAN KANDIDAT RELAWAN MENABUNG POHON PERTAMINA FOUNDATION
+		    </h3>
+		    <hr/>
+		    <p>yang bertanda tangan di bawah ini:</p>
+                                    
+                    <div class="control-group">
+			<label class="control-label" for="inputnama">Nama Lengkap</label>
+                            <div class="controls">
+                                <input type="text" id="inputname" placeholder="nama depan"> <input type="text" id="inputname" placeholder="nama belakang">
+                            </div>
+                    </div>
+		    <div class="control-group">
+                        <label class="control-label" for="kota">Kabupaten/Kota :</label>
+                            <div class="controls">
+                                <input type="text" id="inputkota" placeholder="">
+                            </div>
+                    </div>
+		    <div class="control-group">
+                            <label class="control-label" for="prov">Provinsi :</label>
+                            <div class="controls">
+                                <input type="text" id="inputprov" placeholder="">
+                            </div>
+                    </div>
+		    <div class="control-group">
+                        <label class="control-label" for="ho-ktp">Alamat :</label>
+                             <div class="controls">
+                                    <textarea rows="3"></textarea>
+                                </div> 
+                    </div>
+		    <div class="control-group">
+                            <label class="control-label" for="noktp">No KTP :</label>
+                            <div class="controls">
+                                <input type="text" id="inputprov" placeholder="">
+                            </div>
+                    </div>
+		    <div class="control-group">
+                        <label class="control-label" for="sd">Pindai KTP* :</label>
+                        <div class="controls">
+                            <input type="file" id="inputscanktp" placeholder="">
+                            <p class="tiny">Unggah hasil pindaian KTP dalam format .jpg, .gif atau .png dengan ukuran file maksimal 50 KB</p>
+                        </div>
+                    </div>
+		    <div class="control-group">
+                        <label class="control-label" for="ho">No HP :</label>
+                        <div class="controls">
+                            <input type="text" id="input-hp" placeholder="08571234567">
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="ho-tlp">No Tlp :</label>
+                        <div class="controls">
+                            <input type="text" id="inputno-tlp" placeholder="022-9878467">
+                        </div>
+                    </div>
+		    <div class="control-group">
+                        <label class="control-label" for="email">Email* :</label>
+                        <div class="controls">
+                            <input type="text" id="email" placeholder="sigitsedayu@gmail.com">
+                        </div>
+                    </div>
+		    <div class="control-group">
+                        <label class="control-label" for="ho-tlp">No Akun Sobatbumi :</label>
+                        <div class="controls">
+                            <input class="" type="text" id="inputno-tlp" placeholder="2345">
+                        </div>
+                    </div>
+		    <div class="control-group">
+                        <label class="control-label" for="job">Pekerjaan :</label>
+                        <div class="controls">
+                            <input type="text" id="job" placeholder="">
+                        </div>
+                    </div>
+		    
+		    <p>Mengajukan permohonan untuk menjadi Relawan Gerakan Menabung Pohon Pertamina Foundation dan menyatakan kesanggupan untuk menjalankan sistem Menabung Pohon Pertamina Foundation. Demikian surat ini kami buat sebagai pernyataan ketertarikan kami untuk berpastisipasi dalam Program Menabung Pohon Pertamina Foundation.</p>
+		     <div class="control-group">
+		    <label class="checkbox">
+		     <input type="checkbox"> Saya setuju,
+		    </label>
+		     </div>
+		    <div class="control-group">
+                        <div class="controls">
+                            <button id="submit-profile" type="submit" class="btn btn-info">Simpan</button> <button type="reset" class="btn">batal</button>
+                        </div>
+                    </div>
+                </form>
+                                
+                <div style="display: none;" id="alert-profile-message" class="alert alert-success alert-message"> 
+                    <strong>Success!</strong> Data berhasil disimpan.
+                </div>
+    
+	    </div>
+            
+            
+<!--form relawan registration-->
