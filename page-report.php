55eb7800a9e622ea9ef33e390122c9675ec2531d
kun <kunwidy@gmail.com>
2013-02-05 08:25:50 +0700

    new

--- page-report.php
+++ page-report.php
@@ -7,10 +7,15 @@
 			
 			<div class="span12 well">
 				<table class="span12">
-					<caption>LAPORAN</caption>
-
+					<caption><h3>Laporan Lot : <b>RANCASENGANG7 <br> ( 423497-rancasengang7)</b></h3></caption>
+				</table>
+				
+				<table class="">
 						<tr>
-							<td>Nama Lot</td>
+							<td><h3>Data Lot</h3></td>
+						</tr>
+						<tr>
+							<td class="span3">Nama Lot</td>
 							<td>RANCASENGANG7</td>
 						</tr>
 						<tr>
@@ -32,13 +37,226 @@
 							<td>0,30 hektar</td>
 						</tr>
 						<tr>
-							<td>Pola Kerjsama</td>
-							<td>AA-70/30</td>
+							<td>Jumlah Pohon</td>
+							<td>3.260</td>
+							<td>Jenis Pohon</td>
+							<td>Sengon</td>
 						</tr>
-					
 				</table>
+				
+				<table class="">
+					<tr>
+						<td><h3>Lokasi Lot</h3></td>
+					</tr>
+					<tr>
+						<td class="span3">Desa</td>
+						<td>Rancasengang</td>
+						<td>Kecamatan</td>
+						<td>Sindangkerta</td>
+					</tr>
+					<tr>
+						<td>Kota / Kabupten</td>
+						<td>Kabupaten Bandung Barat</td>
+					</tr>
+					<tr>
+						<td>Titik Koordinat</td>
+						<td>107.451328,  -6.984192</td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+						<td><img width="150" height="150" src="http://twitgreen.com/wp-content/uploads/2013/01/7-1rancasengang-150x150.jpg" class="attachment- wp-post-image" alt="7 1rancasengang" title="7 1rancasengang" /></td>
+						<td>
+												<img src="https://maps.googleapis.com/maps/api/staticmap?path=color:0x0000ff|weight:5|  -6.984192,107.451328|  -6.984192,107.451328&size=320x150&sensor=false&maptype=satellite">
+						</td>
+					</tr>
+
+				</table>
+				
+				<table class="">
+					<tr>
+						<td><h3>Data Relawan</h3></td>
+					</tr>
+					<tr>
+						<td class="span3">Nama</td>
+						<td>Hari Krismis Tiyanto</td>
+					</tr>
+					<tr>
+						<td>No. KTP</td>
+						<td>3275102512630012</td>
+					</tr>
+					<tr>
+						<td>Tempat, tanggal lahir</td>
+						<td>semarang, 25 desember 1963</td>
+					</tr>
+					<tr>
+						<td>Pekerjaan / Instansi</td>
+						<td> / </td>
+					</tr>
+					<tr>
+						<td>Alamat</td>
+						<td>perumahan radiance villa  no A - 40 , jatiranggon , jatisampurna , bekasi</td>
+					</tr>
+					<tr>
+						<td>No. Telp / HP</td>
+						<td>087777000963 / 081283992639</td>
+						<td>No. Akun Sobatbumi</td>
+						<td>2879</td>
+					</tr>
+				</table>
+				
+				<table class="">
+					<tr>
+					<td><h3>Data Verifikator</h3></td>
+					</tr>
+					<tr>
+					<td class="span3">Nama</td>
+					<td>Sigit Sedayu</td>
+					</tr>
+					<tr>
+					<td>No. KTP</td>
+					<td>0912.345.456.78.90</td>
+					</tr>
+					<tr>
+					<td>Tempat, tanggal lahir</td>
+					<td>Bandung, 17 Agustus 1980</td>
+					</tr>
+					<tr>
+					<td>Pekerjaan / Instansi</td>
+					<td> wiraswasta / pertanian</td>
+					</tr>
+					<tr>
+					<td>Alamat</td>
+					<td>Jl. Sindang reret no 123</td>
+					</tr>
+					<tr>
+					<td>No. Telp / HP</td>
+					<td> 022-123456 / 08571235647</td>
+ 					<td>No. Akun Sobatbumi</td>
+					<td>0987</td>
+					</tr>
+				</table>
+				
+				<table>
+				<tr>
+					<td><h3>Data Petani</h3></td>
+				</tr>
+				<tr>
+					<td class="span3">Nama</td>
+					<td>O Sodikin</td>
+				</tr>
+				<tr>
+					<td>No. KTP</td>
+					<td>3217140107540065</td>
+				</tr>
+				<tr>
+					<td>Tempat, tanggal lahir</td>
+					<td>Bandung, 02 Juni 1956 </td>
+				</tr>
+				<tr>
+					<td>Pekerjaan</td>
+					<td>Petani</td>
+				</tr>
+				<tr>
+					<td>Alamat</td>
+					<td>Kp.Pasir Gombong Ds.Rancasengang Kec.Sindangkerta </td>
+				</tr>
+				<tr>
+					<td>No. Telp / HP</td>
+					<td>022-789567/ 081234567</td>
+ 					<td>No. Akun Sobatbumi</td>
+					<td>14820</td>
+				</tr>
+				<tr>
+					<td colspan="2" class="center">
+						Selaku <strong>Petani</strong> yang menguasai sebidang lahan berdasarkan administrasi desa tercatat
+					</td>
+				</tr>
+				<tr>
+				</tr>
+			</table>
+			
+			<table class="">
+				<tr>
+					<td class="span3">Nama Pemilik Lahan</td>
+					<td>O Sodikin</td>
+				</tr>
+				<tr>
+					<td>Luas Lahan</td>
+					<td>0,30</td>
+					<td>Nomot Persil</td>
+					<td></td>
+				</tr>
+				<tr>
+					<td>Lokasi Lahan</td>
+					<td>Rancasengang, Sindangkerta, Kabupaten Bandung Barat</td>
+				</tr>
+			</table>
+				
+			<table class="">
+					<tr>
+						<td><h3>Document dalam lot</h3></td>
+					</tr>
+					<tr>
+						<td class="span3"> Tidak ada document di lot ini </td>
+					</tr>
+			</table>
+			<table class="">
+					<tr>
+						<td><h3>Story dalam lot</h3></td>
+					</tr>
+					<tr>
+						<td class="span3"> Tidak ada story di lot ini </td>
+					</tr>
+			</table>
+			<table class="">
+					<tr>
+						<td><h3 >Note dalam lot</h3></td>
+					</tr>
+						<tr>
+							<td class="span12"> 1.Perubahan status lot -(January 30, 2013) : Status diubah dari Draft ke Offering, oleh Hari Krismis Tiyanto pada tanggal 30 January 2013 20:49 WIB
+							</td>
+						</tr>
+						<tr>
+							<td class="span12"> 2.Lot baru -(January 21, 2013) : Hari Krismis Tiyanto membuat lot ini pada tanggal 21 January 2013 15:37 WIB
+							</td>
+						</tr>
+						
+			</table>
+			<br/><br/>
+			<table class="">
+				<tr>
+					<td><p class="">Demikian Laporan ini dibuat. Bandung 04 Februari 2013 </p></td>
+				</tr>
+			</table>
+			<br/>
+			<table class="">
+				<tr>
+					<td class="center span4">
+						<strong>Relawan</strong>
+						<br><br><br><br>
+						<span>Hari Krismis Tiyanto</span>
+					</td>
+					<td class="center span4">
+						<strong>Verifikator</strong>
+						<br><br><br><br>
+						<span>Sigit Sedayu</span>
+					</td>
+					<td class="center span4">
+						<strong>Petani</strong>
+						<br><br><br><br>
+						<span>O Sodikin</span>
+					</td>
+				</tr>
+				
+			</table>
+			
 			</div>
 			
+		</div>
+		<div class="row">
+			<div class="span12">
+				<button class="btn btn-success"><i class="icon-print"></i>  Simpan</button> <button class="btn btn-success"><i class="icon-print"></i> Print</button> <button class="btn">Back</button>
+			</div>
 		</div>
 	</div>
 <?php
