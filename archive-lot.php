073430d6dd831c1387475b7badd2df379e6e93b0
kun <kunwidy@gmail.com>
2013-02-05 11:04:23 +0700

    Update archive-lot.php

--- archive-lot.php
+++ archive-lot.php
@@ -6,6 +6,7 @@
 		<div class="row">
 			<div class="span12">
 			<h2>DAFTAR LOT :</h2>
+			Lihat <button class="btn btn-success btn-small" type="button"> Semua</button> <button class="btn btn-success btn-small" type="button"> Lot terbuka</button>
 			</div>
 			
 			<div id="big-content" class="span9">
